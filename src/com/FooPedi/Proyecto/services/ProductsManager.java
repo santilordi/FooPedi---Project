ab9c58972753f9e2fbf70859a555fa12c87630cd
santilordi <santilordi569@gmail.com>
2025-03-29 12:46:46 -0300

    implement ProductsManager

--- src/com/FooPedi/Proyecto/services/ProductsManager.java
+++ src/com/FooPedi/Proyecto/services/ProductsManager.java
@@ -1,7 +1,7 @@
 package com.FooPedi.Proyecto.services;
 
 import com.FooPedi.Proyecto.model.Product;
-
+import com.FooPedi.Proyecto.util.ProductNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,19 +16,38 @@ public class ProductsManager {
 
     }
 
-    public Product searchProductById(int id){
+    public Product searchProductById(int id) throws ProductNotFoundException{
+        for (Product product : products) {
+            if (product.getId() == id) {
+                return product;
+            }
+        }
 
+        throw new ProductNotFoundException("Producto con ID: " + id + "no encontrado.");
     }
+    
+    public Product searchProductByName(String name) throws ProductNotFoundException{
+        for (Product product : products) {
+            if (product.getName().equals(name)) {
+                return product;
+            }
+        }
 
-    public Product searchProductByName(String name){
-
+        throw new ProductNotFoundException("Producto con nombre: " + name + "no encontrado.");
     }
 
     public List<Product> listProducts(){
-
+        return new ArrayList<>(products);
     }
 
-    public void updateProduct(int id, double price, int stock){
-
+    public void updateProduct(int id, double price, int stock) throws ProductNotFoundException{
+        
+        for (Product product : products) {
+            if (product.getId() == id) {
+            product.setPrice(price);
+            product.setStock(stock);
+            }
+        }
+        throw new ProductNotFoundException("Producto con ID: " + id + " no encontrado.");
     }
 }
